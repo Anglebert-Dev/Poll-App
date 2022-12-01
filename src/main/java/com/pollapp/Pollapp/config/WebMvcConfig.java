668e8cad02b9b658600c581d965202ec054736ac
Anglebert-Dev <anglebertsh@gmail.com>
2022-12-02 00:26:40 +0200

    Enabling CORS

--- src/main/java/com/pollapp/Pollapp/config/WebMvcConfig.java
+++ src/main/java/com/pollapp/Pollapp/config/WebMvcConfig.java
@@ -1,4 +1,21 @@
 package com.pollapp.Pollapp.config;
 
-public class WebMvcConfig {
+// allow cross origin requests from the react client
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebMvcConfig implements  WebMvcConfigurer {
+    private final  long MAX_AGE_SECS = 3600;
+    @Value("${app.cors.allowedOrigins}")
+    private String[] allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+                .maxAge(MAX_AGE_SECS);
+    }
 }
